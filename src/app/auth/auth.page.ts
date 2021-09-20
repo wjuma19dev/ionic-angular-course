a022f284d3e7027817deb9b7a3cf8150d4dd3844
Wilson Juma <wjuma19dev@gmail.com>
2021-09-19 22:12:00 -0500

    Terminado la pagina de autenticacion, agregamos formulario de plantilla con validaciones de alerta y bloqueo de boton submit si el formulario no es valido

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -10,6 +11,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage {
 
+  public isLogin = true;
+
   constructor(
     private authSvc: AuthService,
     private router: Router,
@@ -18,7 +21,6 @@ export class AuthPage {
 
   onLogin() {
     this.authSvc.login();
-
     this.loadingCtrl.create({
       spinner: 'circles',
       message: 'Please wait...',
@@ -30,8 +32,25 @@ export class AuthPage {
           loading.dismiss();
           this.router.navigateByUrl('/places/tabs/discover');
         }, 2000);
-      })
+      });
+  }
 
+  onSwitchAuthModel() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log({ email, password });
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 
 }
